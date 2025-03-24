05cac7c254b4c6ed25d8011abab4437dd231a107
AD <akshaydwivedi@live.com>
2025-03-24 09:42:43 -0700

    delete app module ts , minor refactor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
--- src/types/player-profile-request.ts
+++ src/types/player-profile-request.ts
@@ -1,4 +1,5 @@
 export interface PlayerProfileRequest {
+  id?: string;
   firstName: string;
 
   lastName: string;
