57186c498db4b6ab4900b813a2be1f2ea8da526c
AD <akshaydwivedi@live.com>
2025-03-25 03:30:49 -0700

    interceptor

--- src/auth.interceptor.ts
+++ src/auth.interceptor.ts
@@ -1,13 +1,29 @@
-import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { inject } from "@angular/core";
-import { UserService } from "./app/services/user.service";
+import { HttpEventType, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { UserService } from './app/services/user.service';
+import { tap } from 'rxjs/operators';
 
-export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
-    // Inject the current `AuthService` and use it to get an authentication token:
-    const authToken = inject(UserService).user.accessToken;
-    // Clone the request to add the authentication header.
+export function authInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // Inject the current `AuthService` and use it to get an authentication token:
+  const authToken = inject(UserService).user.accessToken;
+  // Clone the request to add the authentication header.
+
+  if (authToken) {
     const newReq = req.clone({
       headers: req.headers.append('Authorization', `Bearer ${authToken}`),
     });
-    return next(newReq);
+    return next(newReq).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log(req.url, 'returned a response with status', event.status);
+          console.log(event);
+          
+        }
+      })
+    );
   }
+  return next(req);
+}
