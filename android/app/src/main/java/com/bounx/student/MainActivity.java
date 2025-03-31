7e0b09e4215f73740be8f0cabb164909a6974f8f
Shiva <shiva@startdisrupt.com>
2025-03-31 23:59:08 +0530

    build config for android

---
+++ android/app/src/main/java/com/bounx/student/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bounx.student;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bounx.student',
+  appName: 'bounx-student-v-2',
+  webDir: 'www'
+};
+
+export default config;
