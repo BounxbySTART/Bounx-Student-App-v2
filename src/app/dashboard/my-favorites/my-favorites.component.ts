4ed1bdea08e3b205cb4af1ab68aa02da459d8e7e
yeelean <yeelean@startdisrupt.com>
2025-04-04 13:44:49 +0800

    Dashboard - My Bookings + My Favourites

--- src/app/dashboard/my-favorites/my-favorites.component.ts
+++ src/app/dashboard/my-favorites/my-favorites.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItem, IonAvatar, IonLabel } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-my-favorites',
   templateUrl: './my-favorites.component.html',
   styleUrls: ['./my-favorites.component.scss'],
+  imports: [ IonItem, IonAvatar, IonLabel ],
 })
 export class MyFavoritesComponent  implements OnInit {
 
