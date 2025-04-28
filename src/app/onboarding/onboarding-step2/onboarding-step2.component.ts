3bf29c12bed21c6c47b49f90da2c750df6158e48
AD <akshaydwivedi@live.com>
2025-04-28 12:34:53 -0700

    profile picture selector quality

--- src/app/onboarding/onboarding-step2/onboarding-step2.component.ts
+++ src/app/onboarding/onboarding-step2/onboarding-step2.component.ts
@@ -8,6 +8,7 @@ import {
   IonButton,
 } from '@ionic/angular/standalone';
 import { StepProgressComponent } from 'src/app/general/step-progress/step-progress.component';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-onboarding-step2',
@@ -24,7 +25,7 @@ import { StepProgressComponent } from 'src/app/general/step-progress/step-progre
 })
 export class OnboardingStep2Component implements OnInit {
   navController: NavController = inject(NavController);
-
+  profileImage: string = '../../assets/dummy/profile_pic_user/Ryan.png';
   constructor() {}
 
   ngOnInit() {}
@@ -33,5 +34,39 @@ export class OnboardingStep2Component implements OnInit {
   }
   dismiss() {
     this.navController.pop();
+  }
+  async chooseImage(source: CameraSource) {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      correctOrientation: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    const imageUrl = image.webPath;
+
+    if (imageUrl) {
+      // Can be set to the src of an image now
+      this.profileImage = imageUrl;
+    }
+  }
+  async imageFromGallery() {
+    const image = await Camera.pickImages({
+      quality: 90,
+      correctOrientation: true,
+      limit: 1,
+      width: 512,
+      height: 512,
+    });
+    const imageUrl = image.photos[0].webPath;
+
+    if (imageUrl) {
+      // Can be set to the src of an image now
+      this.profileImage = imageUrl;
+    }
   }
 }
