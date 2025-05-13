8cd33f888b331de6beeb256f0ffcd8dd0cc0e426
Shiva <shiva@startdisrupt.com>
2025-05-13 15:51:57 +0530

    fixes on badge component

--- src/app/profile/badge-latest/badge-latest.component.ts
+++ src/app/profile/badge-latest/badge-latest.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlayerService } from 'src/app/services/player.service';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-badge-latest',
@@ -7,6 +9,7 @@ import { PlayerService } from 'src/app/services/player.service';
   styleUrls: ['./badge-latest.component.scss'],
 })
 export class BadgeLatestComponent  implements OnInit {
+  imageUrl:string=environment.badgeImageUrl;
 @Input() profileId:number = 0;
 badges:any[]=[];
   constructor(public playerService:PlayerService) { }
