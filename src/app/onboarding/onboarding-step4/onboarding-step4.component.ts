26d28ba103af6c27858582c883361fa6bfd6a7d5
AD <akshaydwivedi@live.com>
2025-05-14 14:50:58 -0700

    dismiss step 4

--- src/app/onboarding/onboarding-step4/onboarding-step4.component.ts
+++ src/app/onboarding/onboarding-step4/onboarding-step4.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {
-  IonIcon,
-} from '@ionic/angular/standalone';
+import { IonIcon, AlertController } from '@ionic/angular/standalone';
 import { StepProgressComponent } from 'src/app/general/step-progress/step-progress.component';
 import { OnboardingListPaymentComponent } from '../onboarding-list-payment/onboarding-list-payment.component';
 import { Location } from '@angular/common';
-import { FormControl, FormControlName, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { PlayerService } from 'src/app/services/player.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-onboarding-step4',
@@ -17,29 +22,66 @@ import { PlayerService } from 'src/app/services/player.service';
     IonIcon,
     StepProgressComponent,
     OnboardingListPaymentComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
 })
-export class OnboardingStep4Component  implements OnInit {
-paymentInfoGroup!:FormGroup
-  constructor(public location:Location, public router:Router, public playerService:PlayerService) { }
+export class OnboardingStep4Component implements OnInit {
+  paymentInfoGroup!: FormGroup;
+  profileId: number | undefined;
+  constructor(
+    public location: Location,
+    public router: Router,
+    public playerService: PlayerService,
+    private alertController: AlertController,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.profileId = this.userService.currentProfile.id;
     this.paymentInfoGroup = new FormGroup({
-      cardNumber: new FormControl('',[Validators.required]),
-      name: new FormControl('',[Validators.required]),
-      expiryDate: new FormControl('',[Validators.required]),
-      cvv:new FormControl('',[Validators.required])
-    
+      cardNumber: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      expiryDate: new FormControl('', [Validators.required]),
+      cvv: new FormControl('', [Validators.required]),
     });
   }
 
- 
-navigateToCompletion(){
-  
-  this.router.navigateByUrl('/onboarding-complete');
-}
-  dismiss(){
-this.location.back();
+  navigateToCompletion() {
+    this.router.navigateByUrl('/onboarding-complete');
+  }
+  async dismiss() {
+    const alert = await this.alertController.create({
+      header: 'Cancel Profile creation?',
+      message: `Your data will not be saved and profile will not be created if you cancel sign up process now`,
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            // nothing to do
+          },
+        },
+        {
+          text: 'Yes',
+          role: 'confirm',
+          handler: () => {
+            //something to do
+            if (this.profileId) {
+              this.playerService
+                .playerProfileRemove(this.profileId)
+                .subscribe((res) => {
+                  console.log(res);
+                });
+            }
+            this.router.navigateByUrl('/onboarding-complete');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+  backToPrevious() {
+    this.location.back();
   }
 }
